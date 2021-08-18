07968756f095cbb34dd516eee7f4be90daa10c20
Anton Muntianu <antonlteacup@gmail.com>
2021-08-18 15:28:58 +0300

    change thumb to rhombus

--- lib/audio_video_progress_bar.dart
+++ lib/audio_video_progress_bar.dart
@@ -1006,7 +1006,16 @@ class _RenderProgressBar extends RenderBox {
       final thumbGlowPaint = Paint()..color = thumbGlowColor;
       canvas.drawCircle(center, thumbGlowRadius, thumbGlowPaint);
     }
-    canvas.drawCircle(center, thumbRadius, thumbPaint);
+    var path = Path()
+      ..moveTo(thumbDx - thumbRadius, localSize.height / 2)
+      ..lineTo(thumbDx, localSize.height / 2 + thumbRadius)
+      ..lineTo(thumbDx + thumbRadius, localSize.height / 2)
+      ..lineTo(thumbDx, localSize.height / 2 - thumbRadius)
+      ..lineTo(thumbDx - thumbRadius, localSize.height / 2);
+    canvas.drawPath(
+      path,
+      thumbPaint,
+    );
   }
 
   double _proportionOfTotal(Duration duration) {
